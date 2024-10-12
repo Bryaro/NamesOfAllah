0ff51d42a10a30c25401f459a3886dcafce1137f
Bryar Osman <bryarosman.bo@gmail.com>
2024-10-12 11:55:15 +0000

    allowed host for heroku added

--- names99/settings.py
+++ names99/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure--uc#g$u&92qszmg+t+w-e@3pdb77wn6wpuhgm16v5q#)$j$$m9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-heroku-app.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['namesofallah-e548f12d4886.herokuapp.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
